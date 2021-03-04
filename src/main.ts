d63455bc75840a8cb0157c54acd23a79ca3d0ee5
Lorenzo García Rivera <lorenzogrv@mareagestion.com>
2021-03-04 18:34:51 +0100

    Primeros tests, fizzBuzz and friends

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+function main (): void {
+  console.log('Hola mundo')
+}
+
+main()
---
+++ test/suite.test.ts
@@ -0,0 +1,23 @@
+import { fizzBuzz } from '../src/es/geekshubs/academy/kata1/geekshubs'
+
+/* eslint-env jest */
+
+describe('Testing', () => {
+  test('Un test por aqui', () => {
+    // Arrange
+
+    // Act
+
+    // Assert
+    throw new Error('TODO')
+  })
+
+  test('Un test por alla', () => {
+    // Arrange
+
+    // Act
+
+    // Assert
+    throw new Error('TODO')
+  })
+})
